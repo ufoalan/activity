c770845415006c1ecfcc92379c70dade25358b99
Alan Lee <allee@allee-mbp.apac.tibco.com>
2017-11-22 15:31:41 +0800

    add HelloWorld

---
+++ HelloWorld/activity.go
@@ -0,0 +1,39 @@
+package HelloWorld
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger which we'll use to log
+var log = logger.GetLogger("activity-helloworld")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+
+	// do eval
+	input := context.GetInput("input").(string)
+
+	// Use the log object to log the greeting
+	log.Debugf("The Flogo engine says [%s]", input)
+
+	// Set the result as part of the context
+	context.SetOutput("output", "The Flogo engine says "+input)
+
+	return true, nil
+}
