aa2270878d528dfa42c4871d70e632cfa43bf3c1
ufoalan <30685383+ufoalan@users.noreply.github.com>
2017-11-22 12:13:13 +0800

    Update activity.go

--- motor/activity.go
+++ motor/activity.go
@@ -1,7 +1,13 @@
 package motor
 
 import (
-	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"sync"                                 
+        "time"         
+        "fmt"                                                                          
+         
+        "github.com/TIBCOSoftware/flogo-lib/core/activity"
+        "github.com/TIBCOSoftware/flogo-lib/logger"
+        "github.com/ev3go/ev3dev"    
 )
 
 // MyActivity is a stub for your Activity implementation
@@ -23,6 +29,68 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
 
 	// do eval
+        // Get the activity data from the context
+        action := context.GetInput("action").(string)
+        speed := context.GetInput("speed").(int)                                                
 
-	return true, nil
+        // Use the log object to log the greeting                   
+        log.Debugf("The Flogo engine says motor [%s] at [%s] speed", action, speed)
+                        
+                // Get the handle for the medium motor on outA.
+        outA, err := ev3dev.TachoMotorFor("outA", "lego-ev3-l-motor")
+        if err != nil {                  
+                log.Debugf("failed to find large motor on outA: %v", err)
+        }                                          
+        err = outA.SetStopAction("brake").Err()
+        if err != nil {                                 
+                log.Debugf("failed to set brake stop for large motor on outA: %v", err)
+        }                                                                           
+        maxMedium := outA.MaxSpeed()
+                                                         
+        if (counterName == "start") {   
+                outA.SetSpeedSetpoint(50 * maxMedium / 100).Command("run-forever")   
+                checkErrors(outA)
+        } else if (counterName == "stop") {                                                  
+                outA.Command("stop")
+                checkErrors(outA)
+        } else {
+                for i := 0; i < 2; i++ {
+ 
+                        // Run medium motor on outA at speed 50, wait for 0.5 second and then brake.
+                        outA.SetSpeedSetpoint(50 * maxMedium / 100).Command("run-forever")
+                        time.Sleep(time.Second / 2)
+                        outA.Command("stop")
+                        checkErrors(outA)
+ 
+                        // Run medium motor on outA at speed -75, wait for 0.5 second and then brake.
+                        outA.SetSpeedSetpoint(-75 * maxMedium / 100).Command("run-forever")
+                        time.Sleep(time.Second / 2)
+                        outA.Command("stop")
+                        checkErrors(outA)
+                }
+        }
+ 
+        // Set the result as part of the context
+        context.SetOutput("result", "The Flogo engine says motor "+action+" at "+speed+" speed")
+ 
+        // Signal to the Flogo engine that the activity is completed
+ 
+        return true, nil
+}
+
+func checkErrors(devs ...ev3dev.Device) {
+        for _, d := range devs {                                                            
+                err := d.(*ev3dev.TachoMotor).Err()                               
+                if err != nil {            
+                        drv, dErr := ev3dev.DriverFor(d)
+                        if dErr != nil {
+                                drv = fmt.Sprintf("(missing driver name: %v)", dErr)
+                        }                                                                
+                        addr, aErr := ev3dev.AddressOf(d)                          
+                        if aErr != nil {                                           
+                                drv = fmt.Sprintf("(missing port address: %v)", aErr)
+                        }                    
+                        log.Fatalf("motor error for %s:%s on port %s: %v", d, drv, addr, err)
+                }                     
+        }                                
 }
