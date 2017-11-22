10171e3965e4a4b6f4c4c46b06fe3b9d1160cf9d
Alan Lee <allee@allee-mbp.apac.tibco.com>
2017-11-22 15:58:47 +0800

    add new_counter

---
+++ new_counter/activity_test.go
@@ -0,0 +1,54 @@
+package new_counter
+
+import (
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"io/ioutil"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestReadState(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("method", "Read State")
+	tc.SetInput("pin number", 10)
+	//eval
+	_, err := act.Eval(tc)
+	if err != nil {
+		log.Errorf("Error occured: %+v", err)
+	}
+	val := tc.GetOutput("result")
+	log.Debugf("Resut %s", val)
+
+}
